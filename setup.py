31d109461a1b3dced118587ca1d2b8dcc97bd595
Arjun <niteshkrjhag@gmail.com>
2026-06-25 00:14:33 +0530

    wrote setup.py code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+# this will install the package so i can use my local package easily anywhere in my project
+setup(
+    name = 'Wellness AI Project',
+    versio = '0.0.0',
+    author = 'Nitesh Kumar Jha',
+    author_email = 'niteshkrjhag@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+)
